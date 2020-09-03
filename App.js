f93edec6fe6574ce89431b5e7a9cde57c862093d
Skylar Thomas <skylarth@usc.edu>
2020-09-02 19:01:13 -0700

    new project

---
+++ App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {useRef} from 'react';
+import { Component, useState, useEffect } from 'react';
+import { useFonts } from '@use-expo/font';
+import { StyleSheet, Text, View, Image, ImageBackground, Animated, Easing, StatusBar, FlatList, SafeAreaView, ScrollView, TouchableWithoutFeedback, TouchableOpacity, Dimensions } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={[styles.container, styles.centerContent]}>
+        {/* First shadow variant */}
+        <View style={[styles.box, styles.centerContent, styles.shadow1]}>
+          <View style={styles.ball} />
+        </View>
+
+        {/* Second shadow variant */}
+        <View style={[styles.box, styles.centerContent, styles.shadow2]}>
+          <View style={styles.ball} />
+        </View>
+
+        {/* Third shadow variant */}
+        <View style={[styles.box, styles.centerContent, styles.shadow3]}>
+          <View style={styles.ball} />
+        </View>
+      </View>
+  );
+}
+
+function elevationShadowStyle(elevation) {
+  return {
+    elevation,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 0.5 * elevation },
+    shadowOpacity: 0.1,
+    shadowRadius: 0.8 * elevation
+  };
+}
+
+const styles = StyleSheet.create({
+  shadow1: elevationShadowStyle(5),
+  shadow2: elevationShadowStyle(10),
+  shadow3: elevationShadowStyle(20),
+  box: {
+    borderRadius: 8,
+    backgroundColor: 'white',
+    padding: 24
+  },
+
+  // supporting styles
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    backgroundColor: '#ecf0f1',
+    padding: 24
+  },
+  centerContent: {
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  ball: {
+    borderRadius: 128,
+    width: 128,
+    height: 128,
+    backgroundColor: 'lightblue'
+  }
+});
