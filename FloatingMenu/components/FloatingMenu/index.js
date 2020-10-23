ad74c59fc23125bd911cd4d3c12ac7173dd0fc01
Skylar Thomas <skylarth@usc.edu>
2020-10-22 21:31:40 -0700

    color combos

--- FloatingMenu/components/FloatingMenu/index.js
+++ FloatingMenu/components/FloatingMenu/index.js
@@ -352,6 +352,7 @@ class FloatingMenu extends React.PureComponent {
             applyButtonInnerWidthFirst(innerWidth),
             {
               flexDirection: 'column',
+              position: 'absolute',
               display: global.lastColor != 'transparent' ? 'flex' : 'none',
               //THIS CONTROLS SPLIT COLOR
               backgroundColor:
